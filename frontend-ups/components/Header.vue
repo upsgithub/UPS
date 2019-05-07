b32237a9fbe1146e18534e6aa4f8f0d45d636f80
Vildmusen <viktorvilmusenaho@hotmail.com>
2019-05-07 16:29:39 +0200

    mobile first nu hehe

--- frontend-ups/components/Header.vue
+++ frontend-ups/components/Header.vue
@@ -43,70 +43,141 @@
 <style lang="scss">
     .menu{
         background: #30242e;
-        padding: 15px 0px;
         text-align: center;
         position: absolute;
         top: 0;
 
         &-item{
-            position: relative;
-            text-align: center;
-            max-width: 135px;
-            margin-top: 20px;
-            float:left;
-            &::after{
-                clear: both;
-            }
-
-            img{
-                width: 30%;
-                height: auto;
-            }
+            display: none;
         }
     }
-
+    
     .wrapper{
         display: inline-block;
     }
 
     .logo{
         padding: 0;
-        max-width: 270px;
+        width: auto;
         float: left;
         &::after{
             clear: both;
         }
     }
-    
+
     .logo img{
-        width: 100%;
+        margin-bottom: -5px;
+        width: 70%;
     }
 
-    .text h2{
-        padding: 0;
-        margin: 0;
-        color: #eb5e43;
-        font-size: 16px;
-        font-weight: 100;
-    }
-
-    @media only screen and (max-width: 800px) {
+    @media only screen and (min-width: 800px) {
         .menu{
-            height: 100px;
-            padding: 0;
+        padding: 7.5px 0px;
+        height: 120px;
+        
+            &-item{
+                display: block;
+                position: relative;
+                text-align: center;
+                max-width: 145px;
+                margin-top: 20px;
+                float:left;
+                &::after{
+                    clear: both;
+                }
+
+                img{
+                    width: 30%;
+                    height: auto;
+                }
+            }
         }
 
         .logo{
-            width: auto; 
-            height: 150px;
+            margin-top: 10px;
+            width: 33.3%;
         }
 
         .logo img{
-            max-width: 270px;
+            width: auto;
+            max-height: 90px;
         }
 
-        .menu-item{
-            display: none;
+        .text h2{
+            padding: 0;
+            margin: 0;
+            color: #eb5e43;
+            font-size: 16px;
+            font-weight: 100;
         }
     }
+
+    // .menu{
+    //     background: #30242e;
+    //     padding: 15px 0px;
+    //     text-align: center;
+    //     position: absolute;
+    //     top: 0;
+
+    //     &-item{
+    //         position: relative;
+    //         text-align: center;
+    //         max-width: 135px;
+    //         margin-top: 20px;
+    //         float:left;
+    //         &::after{
+    //             clear: both;
+    //         }
+
+    //         img{
+    //             width: 30%;
+    //             height: auto;
+    //         }
+    //     }
+    // }
+
+    // .wrapper{
+    //     display: inline-block;
+    // }
+
+    // .logo{
+    //     padding: 0;
+    //     max-width: 270px;
+    //     float: left;
+    //     &::after{
+    //         clear: both;
+    //     }
+    // }
+    
+    // .logo img{
+    //     width: 100%;
+    // }
+
+    // .text h2{
+    //     padding: 0;
+    //     margin: 0;
+    //     color: #eb5e43;
+    //     font-size: 16px;
+    //     font-weight: 100;
+    // }
+
+    // @media only screen and (max-width: 800px) {
+    //     .menu{
+    //         height: 100px;
+    //         padding: 0;
+    //     }
+
+    //     .logo{
+    //         width: auto; 
+    //         height: 150px;
+    //     }
+
+    //     .logo img{
+    //         max-width: 270px;
+    //     }
+
+    //     .menu-item{
+    //         display: none;
+    //     }
+    // }
 </style>
