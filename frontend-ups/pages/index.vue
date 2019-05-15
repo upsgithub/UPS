dcc4f62c3bfc6bae61e9999f56a0f955ece98d00
Vildmusen <viktorvilmusenaho@hotmail.com>
2019-05-15 15:50:34 +0200

    post layout fixad, men bilder som är 'för små' för postsen lägger sig fult

--- frontend-ups/pages/index.vue
+++ frontend-ups/pages/index.vue
@@ -1,22 +1,38 @@
 <template>
     <section class="container">
         <Header />
-        <div class="container-content">   
-            <div class="posts">
-              <h1> Posts from rest api</h1>
-              <div class="post" v-for="post in posts" :key="post.id">
-                <h4>{{ post.title.rendered }}</h4>
-                <div class="post-content" v-html="post.excerpt.rendered"></div>
-              </div>
+        <div class="container-content">
+            <div class="posts container--full">
+                <div class="post col-12">
+                    <div class="post-title">
+                        <h1> Vilka är vi? </h1>
+                    </div>
+                    <div class="post-text">
+                        <p> 
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum ex sit amet justo feugiat viverra. Pellentesque sit amet velit tempor, euismod augue viverra, laoreet mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris vel ipsum vitae mi maximus rhoncus eu nec ex. In sollicitudin ut elit semper elementum. Praesent vitae magna non arcu imperdiet tempor id quis libero. Nunc id tempus neque. Pellentesque ac leo sapien.
+                            Curabitur gravida vulputate sapien, et semper odio sagittis eu. 
+                        </p>
+                    </div>
+                    <button>Mer om oss</button>
+                </div>
+                <div class="post-picture col-5">
+                    <img src="../assets/img/placeholder_img.png" /> 
+                </div>
             </div>
-            <button>Test</button>
+
             <Sponsor />
-            <div class="posts">
-              <h1> Posts from rest api</h1>
-              <div class="post" v-for="post in posts" :key="post.id">
-                <h4>{{ post.title.rendered }}</h4>
-                <div class="post-content" v-html="post.excerpt.rendered"></div>
-              </div>
+            <div class="posts container--full">
+                <div class="posts" v-for="post in posts" :key="post.id">
+                    <div class="post-picture col-5">
+                        <img src="../assets/img/placeholder_img.png" /> 
+                    </div>
+                    <div class="post col-12">
+                        <div class="post-title">
+                        <h1>{{ post.title.rendered }}</h1>
+                        </div>
+                        <div class="post-text" v-html="post.excerpt.rendered"></div>
+                    </div>
+                </div>
             </div>
         </div>
         <br><br>
@@ -60,4 +76,13 @@ export default {
 </script>
 
 <style lang="scss">
+.posts{
+    display: inline-block;
+}
+
+@media only screen and (min-width: 800px) {
+    .posts{
+        margin-top: 20px;
+    }
+}
 </style>
