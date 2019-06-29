d4cfe475416a33e09fe8a6a5dfb527dfc91b8725
esplito <eesplund@gmail.com>
2019-06-29 18:10:18 +0200

    Reconfigured slideshow component

--- frontend-ups/components/Slideshow.vue
+++ frontend-ups/components/Slideshow.vue
@@ -1,161 +1,140 @@
 <template>
-    <div class="slider-wrapper">
-        <div id="jssor_1">
-            <!-- Loading Screen -->
-            <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
-                <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="../assets/slider/img/spin.svg" />
+    <div class="slideshow">
+        <!-- Slider main container -->
+        <div class="swiper-container">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <nuxt-link to="/utskotten">
+                        <img srcset="~assets/img/globe_new@320w.jpeg 320w,
+                        ~assets/img/globe_new@480w.jpeg 480w,
+                        ~assets/img/globe_new@768w.jpeg 768w,
+                        ~assets/img/globe_new@1024w.jpeg 1024w,
+                        ~assets/img/globe_new@1376w.jpeg 1376w,
+                        ~assets/img/globe_new@1920w.jpeg 1920w"
+                        sizes="auto"
+                        src="~assets/img/globe_new@1920w.jpeg" alt="globe"/>
+                    </nuxt-link>
+                </div>
+                <div class="swiper-slide">
+                    <nuxt-link to="/forening">
+                        <img srcset="~assets/img/globe_new@320w.jpeg 320w,
+                        ~assets/img/globe_new@480w.jpeg 480w,
+                        ~assets/img/globe_new@768w.jpeg 768w,
+                        ~assets/img/globe_new@1024w.jpeg 1024w,
+                        ~assets/img/globe_new@1376w.jpeg 1376w,
+                        ~assets/img/globe_new@1920w.jpeg 1920w"
+                        sizes="auto"
+                        src="~assets/img/globe_new@1920w.jpeg" alt="globe"/>
+                    </nuxt-link>
+                </div>
+                <div class="swiper-slide">
+                    <nuxt-link to="/student">
+                        <img srcset="~assets/img/globe_new@320w.jpeg 320w,
+                        ~assets/img/globe_new@480w.jpeg 480w,
+                        ~assets/img/globe_new@768w.jpeg 768w,
+                        ~assets/img/globe_new@1024w.jpeg 1024w,
+                        ~assets/img/globe_new@1376w.jpeg 1376w,
+                        ~assets/img/globe_new@1920w.jpeg 1920w"
+                        sizes="auto"
+                        src="~assets/img/globe_new@1920w.jpeg" alt="globe"/>
+                    </nuxt-link>
+                </div>
             </div>
-            <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:984px;height:380px;overflow:hidden;">
-                <div>
-                    <img data-u="image" src="../assets/slider/img/001.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="../assets/slider/img/002.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="../assets/slider/img/003.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="../assets/slider/img/004.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="../assets/slider/img/005.jpg" />
-                </div>
-            </div>
-            <!-- Bullet Navigator -->
-            <div data-u="navigator" class="jssorb051" style="position:absolute;bottom:12px;right:12px;" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
-                <div data-u="prototype" class="i" style="width:16px;height:16px;">
-                    <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-                        <circle class="b" cx="8000" cy="8000" r="5800"></circle>
-                    </svg>
-                </div>
-            </div>
-            <!-- Arrow Navigator -->
-            <div data-u="arrowleft" class="jssora051" style="width:55px;height:55px;top:0px;left:25px;" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
-                <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-                    <polyline class="a" points="11040,1920 4960,8000 11040,14080 "></polyline>
-                </svg>
-            </div>
-            <div data-u="arrowright" class="jssora051" style="width:55px;height:55px;top:0px;right:25px;" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
-                <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-                    <polyline class="a" points="4960,1920 11040,8000 4960,14080 "></polyline>
-                </svg>
-            </div>
+            <!-- If we need pagination -->
+            <div class="swiper-pagination"></div>
+
+            <!-- If we need navigation buttons -->
+            <div class="swiper-button-prev swiper-button-white"></div>
+            <div class="swiper-button-next swiper-button-white"></div>
+
+            <!-- If we need scrollbar -->
+            <div class="swiper-scrollbar"></div>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-    head: {
-        script: [
-            { 
-                src: 'https://code.jquery.com/jquery-1.12.4.js',
-                type: 'text/javascript'
+import Swiper from 'swiper';
+    
+export default{
+    mounted(){
+        var mySwiper = new Swiper ('.swiper-container', {
+            // Optional parameters
+            speed: 800,
+            slidesPerView: 1,
+            centeredSlides: true,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: true,
             },
-            { 
-                src: '/jssor.slider-27.5.0.min.js',
-                type: 'text/javascript'
-            }             
-        ]
-    },
-    mounted:function(){
-        this.runSlider()
-    },
-    methods: {   
-        runSlider:function(){
+            loop: true,
+            // If we need pagination
+            pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+            },
 
-            var jssor_1_SlideshowTransitions = [{$Duration:800,$Opacity:2}];
-
-            var jssor_1_options = {
-                $AutoPlay: 1,
-                $SlideshowOptions: {
-                    $Class: $JssorSlideshowRunner$,
-                    $Transitions: jssor_1_SlideshowTransitions,
-                    $TransitionsOrder: 1
-                },
-                $ArrowNavigatorOptions: {
-                    $Class: $JssorArrowNavigator$
-                },
-                $BulletNavigatorOptions: {
-                    $Class: $JssorBulletNavigator$
-                }
-            };
-
-            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-            /*#region responsive code begin*/
-
-            var MAX_WIDTH = 1200;
-
-            function ScaleSlider() {
-                var containerElement = jssor_1_slider.$Elmt.parentNode;
-                var containerWidth = containerElement.clientWidth;
-
-                if (containerWidth) {
-
-                    var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
-
-                    jssor_1_slider.$ScaleWidth(expectedWidth);
-                }
-                else {
-                    window.setTimeout(ScaleSlider, 30);
-                }
-            }
-
-            ScaleSlider();
-
-            $(window).bind("load", ScaleSlider);
-            $(window).bind("resize", ScaleSlider);
-            $(window).bind("orientationchange", ScaleSlider);
-        }
+            // Navigation arrows
+            navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+            },
+        })
     }
 }
+
 </script>
 
 <style lang="scss">
-.slider-wrapper{
-    margin: auto 0;
-    overflow: hidden; 
-    // margin-top: -20px;
-    // margin-left: -20px;
-    // margin-right: -20px;
+.slideshow{
+    height: 280px;
+    width: 100%;
 }
-
-#jssor_1{
-    margin: 0 auto;
-    position:relative;
-    top:0px;
-    left:0px;
-    width:984px;
-    height:280px;
-    max-height: 320px;
-    overflow:hidden;
-    visibility:hidden;
+.swiper-container{
+    height: 100%;
+    width: 100%;
+    margin-left: auto;
+    margin-right: auto;
 }
-
-.jssorl-009-spin img {
-    animation-name: jssorl-009-spin;
-    animation-duration: 1.6s;
-    animation-iteration-count: infinite;
-    animation-timing-function: linear;
+.swiper-pagination{
+    &-bullet{
+        width: 10px;
+        height: 10px;
+        border: 1px solid #eee;
+        background: #EB5E43;
+    }
 }
-
-@keyframes jssorl-009-spin {
-    from { transform: rotate(0deg); }
-    to { transform: rotate(360deg); }
+.swiper-slide {
+    display: -webkit-box;
+      display: -ms-flexbox;
+      display: -webkit-flex;
+      display: flex;
+      -webkit-box-pack: center;
+      -ms-flex-pack: center;
+      -webkit-justify-content: center;
+      justify-content: center;
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      -webkit-align-items: center;
+      align-items: center;
 }
-
-/*jssor slider bullet skin 051 css*/
-.jssorb051 .i {position:absolute;cursor:pointer;}
-.jssorb051 .i .b {fill:#fff;fill-opacity:0.5;}
-.jssorb051 .i:hover .b {fill-opacity:.7;}
-.jssorb051 .iav .b {fill-opacity: 1;}
-.jssorb051 .i.idn {opacity:.3;}
-
-/*jssor slider arrow skin 051 css*/
-.jssora051 {display:block;position:absolute;cursor:pointer;}
-.jssora051 .a {fill:none;stroke:#fff;stroke-width:360;stroke-miterlimit:10;}
-.jssora051:hover {opacity:.8;}
-.jssora051.jssora051dn {opacity:.5;}
-.jssora051.jssora051ds {opacity:.3;pointer-events:none;}
+.swiper-button-prev {
+    left: 20px;
+}
+.swiper-button-next{
+    right: 20px;
+}
+@media only screen and (min-width: 768px) {
+    .swiper-button-prev {
+        left: 40px;
+    }
+    .swiper-button-next{
+        right: 40px;
+    }
+    .slideshow{
+        height: 380px;
+    }
+}
 </style>
