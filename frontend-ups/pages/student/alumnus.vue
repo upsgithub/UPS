bae0932e03fccec928f249887664b57abc2b2b9d
Bornegrim <lucas.bornegrim.9005@student.uu.se>
2019-07-10 16:36:22 +0200

    alumnus

---
+++ frontend-ups/pages/student/alumnus.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <page />
+    </div>        
+</template>
+
+<script>
+import page from '~/components/PlainPage.vue'
+
+export default {
+    components: {
+        page
+    }
+}                
+</script>
+
+<style lang="scss">
+
+</style>
