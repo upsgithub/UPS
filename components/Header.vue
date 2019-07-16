64fdfa200e871b565231f361388d760a3f3f783c
Bornegrim <lucas.bornegrim@gmail.com>
2019-07-16 15:54:04 +0200

    nästan klar, liten bugg

--- components/Header.vue
+++ components/Header.vue
@@ -57,7 +57,6 @@
                     <div class="desktop-item" @click="hide('forening')" v-for="page in forening" :key="page.id">
                         <h3><nuxt-link v-bind:to="'/foreningen/'+ page.slug ">{{ page.title.rendered }}</nuxt-link></h3>
                     </div>
-                    <div class="desktop-item" @click="hide('forening')"><h3><nuxt-link to="/foreningen/policy">Policies</nuxt-link></h3></div>
                     <div class="desktop-item" @click="hide('forening')"><h3><a href="https://drive.google.com/drive/u/1/folders/0B4DkCw-cVaitcWlURFZrb2VmeDQ" target="blank">Mötesprotokoll</a></h3></div>
                     <div class="desktop-item inner" ref="inner" @mouseover="show('utskotten')" @mouseleave="hide('utskotten')">
                         <h3 v-if="english"><nuxt-link to="/foreningen">Committees</nuxt-link></h3>
