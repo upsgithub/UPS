e02fba8d1d4550b93aa60faadbda6bd4316d6640
Vildmusen <viktorvilmusenaho@hotmail.com>
2019-07-16 22:10:37 +0200

    student/utbildning med sammanfattningar till alla undersidor

--- pages/student/index.vue
+++ pages/student/index.vue
@@ -1,180 +1,19 @@
 <template>
     <div>
-        <sync-loader v-if="this.$store.state.loading" class="vue-spinner" :loading="this.$store.state.loading" :color="color"></sync-loader>
-        <div v-else-if="cur_page"  class="banner-background">
-            <img v-if="!cur_page.acf.banner_bild" data-srcset="/img/people_books@320w.jpg 320w,
-           /img/people_books@480w.jpg  480w, 
-            /img/people_books@768w.jpg 768w,
-            /img/people_books@1024w.jpg  1024w, 
-            /img/people_books@1376w.jpg  1376w, 
-            /img/people_books@1920w.jpg  1920w"
-                    data-sizes="auto"
-                    data-src="/img/people_books@1920w.jpg" class="lazyload" alt="people"/>
-            <img v-if="cur_page.acf.banner_bild" :data-srcset="cur_page.acf.banner_bild.sizes.medium + ' 320w,' +
-                    cur_page.acf.banner_bild.sizes.medium_large + ' 768w,' +
-                    cur_page.acf.banner_bild.sizes.large + ' 1024w,' +
-                    cur_page.acf.banner_bild.url + ' 1920w'"
-                    data-sizes="auto"
-                    :data-src="cur_page.acf.banner_bild.url + '?lqip'" class="lazyload" :alt="cur_page.acf.banner_bild.alt"/>
-            <h3 v-if="cur_page.title" :style="cur_page.acf.bannertext">{{ cur_page.title.rendered }}</h3>
-        </div>
-
-        <div class="content-wrapper">
-            
-            <div class="plain container--full">     
-                <div class="plain-text col-12">
-                <sync-loader v-if="loading" class="vue-spinner" :loading="loading" :color="color"></sync-loader>
-                    <div v-else-if="cur_page" class="post-title">
-                    
-                        <h1> {{ cur_page.title.rendered }} </h1>
-                        
-                    </div>
-                    <div v-if="cur_page" class="post-text" v-html="cur_page.content.rendered"></div>
-                </div>
-            </div>
-            
-            <KommandeEvent />
-            <Sponsor />
-        </div>
-        <Instagram />
+        <motherpage />
     </div>        
 </template>
 
 <script>
-import Sponsor from '~/components/Sponsor.vue'
-import Instagram from '~/components/Instagram.vue'
-import KommandeEvent from '~/components/kommandeEvent.vue'
-import SyncLoader from 'vue-spinner/src/SyncLoader.vue'
-import {store} from '../../store/index.js'
-import axios from 'axios'
+import motherpage from '~/components/motherpage.vue'
 
 export default {
-    data() {
-        return {
-            color: '#eb5e43'
-        }
-    },
-    async mounted() {
-        await this.$store.cache.dispatch('get_allPages');
-    },
-    methods: {
-        current_url:function(){
-            return "student"
-        },
-        current_page:function(pagesArr, url){
-            for(var i = 0; i < pagesArr.length; i++){
-                if(pagesArr[i].slug == url){
-                    return pagesArr[i];
-                }
-            }
-        }
-    },
-    computed:{
-        cur_page(){
-            return this.current_page(this.$store.state.pages, this.current_url());
-        },
-        loading(){
-            return this.cur_page == undefined;
-        }
-    },
     components: {
-        Sponsor,
-        Instagram,
-        KommandeEvent,
-        SyncLoader
+       motherpage
     }
 }
 </script>
 
 <style lang="scss">
-.banner{
-    text-align: center;
-    display: inline-block;
 
-    &-background{
-        height: 150px;
-        text-align: center;
-        font-size: 20px;
-        position: relative;
-        overflow: hidden;
-        
-
-        > h3, img{
-            position: absolute;
-            top: 50%;
-            transform: translate(-50%, -50%);
-            left: 50%;
-        }
-        h3{
-            color: #fff;
-            z-index: 2;
-            text-shadow: 0px 0px 8px rgba(50, 50, 50, 0.9);
-        }
-
-        img{
-            width: 100%;
-        }
-    }
-
-    &-kontakt{
-        margin-bottom: 15px;
-        h4{
-            margin: 10px 0;
-        }
-        h5{
-            margin: 15px 0;
-            font-weight: 50;
-        }
-    }
-
-    &-picture{
-        border-radius: 50%;
-		overflow: hidden;
-		width: 160px;
-		height: 160px;
-        position: relative;
-        margin: 5px auto 20px auto;
-
-        img{
-            max-width: 100%;
-            height: auto;
-
-            &::after{
-                content: "";
-                clear: both;
-                display: table;
-            }
-        }
-    }
-}
-.wp-block-image{
-    margin: 20px 0;
-}
-
-@media only screen and (min-width: 800px) {
-    .banner{
-        margin: 20px 0;
-
-        &-background{
-            height: 280px;
-            padding-top: 0px;
-        } 
-
-        &-text{
-            text-align: left;
-            width: 75%;
-            float: left;
-        }
-
-        &-kontakt{
-            width: 25%;
-            float: left;
-        }
-    }
-}
-.plain{
-    &-background{
-        background-image: url("../../assets/img/plain_placeholder.jpg");
-    }
-}
 </style>
