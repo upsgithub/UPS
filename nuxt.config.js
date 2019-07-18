c7e87c4335428d66e451761b9ffcdcc7e6f1b0c1
Vildmusen <viktorvilmusenaho@hotmail.com>
2019-07-18 18:00:49 +0200

    bytte favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: 'Välkommen till Uppsala Politices Studernades hemsida. Här kan du hitta information om vår förening och våra program på Uppsala Universitet.'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
 
