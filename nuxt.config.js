e247e658cef9145d95b94013b7b4e1dd7a2af0a1
Vildmusen <viktorvilmusenaho@hotmail.com>
2019-07-18 22:10:59 +0200

    page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Uppsala Politices Studerande',
+    title: 'Uppsala Politicesstuderande',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
